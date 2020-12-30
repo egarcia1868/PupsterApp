04326f619295dd44f36b87f630e0e123663f04d8
Eric Garcia <egarcia1868@gmail.com>
2020-12-30 16:11:35 -0600

    now utilizing react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import React from "react";
-import PupsterContainer from "./components/PupsterContainer";
+import React, { useState } from "react";
+// import PupsterContainer from "./components/PupsterContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./pages/About";
+import Discover from "./pages/Discover";
+import Search from "./pages/Search";
 
-const App = () => <PupsterContainer />;
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState()
+  
+  const handlePageChange = (page) => {
+    setCurrentPage({ page })
+  }
+  return (
+      <Router>
+        <div>
+          <NavTabs 
+            handlePageChange={handlePageChange}
+            currentPage ={currentPage}
+          />
+          <Route exact path="/" component={About}/>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/discover" component={Discover}/>
+          <Route exact path="/search" component={Search}/>
+        </div>
+      </Router>
+  )
+};
 
 export default App;
--- src/components/NavTabs/index.js
+++ src/components/NavTabs/index.js
@@ -1,45 +1,88 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavTabs = (props) => {
   return (
-    <ul className="nav nav-tabs">
-      <li className="navbar-brand" style={{paddingLeft:"1%"}}>
-  {/* I see repeated code here.  I believe I can simplify this later on  */}
-        <a
-          href="#about"
-          onClick={() => props.handlePageChange("About")}
-        >
-          Pupster
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => props.handlePageChange("About")}
-          className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#discover"
-          onClick={() => props.handlePageChange("Discover")}
-          className={props.currentPage === "Discover" ? "nav-link active" : "nav-link"}
-        >
-          Discover
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#search"
-          onClick={() => props.handlePageChange("Search")}
-          className={props.currentPage === "Search" ? "nav-link active" : "nav-link"}
-        >
-          Search
-        </a>
-      </li>
-    </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" 
+        to="/about"
+        onClick={() => props.handlePageChange("about")}
+      >
+        Pupster
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/about"
+              onClick={() => props.handlePageChange("about")}
+              className={
+                props.currentPage === "/" || props.currentPage === "about"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/discover"
+              onClick={() => props.handlePageChange("discover")}
+              className={
+                props.currentPage === "discover" ? "nav-link active" : "nav-link" }
+            >
+              Discover
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/search"
+              onClick={() => props.handlePageChange("search")}
+              className={
+                props.currentPage === "search" ? "nav-link active" : "nav-link" }
+            >
+              Search
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  //   <ul className="nav nav-tabs">
+  //     <li className="navbar-brand" style={{paddingLeft:"1%"}}>
+  // {/* I see repeated code here.  I believe I can simplify this later on  */}
+  //       <a
+  //         href="#about"
+  //       >
+  //         Pupster
+  //       </a>
+  //     </li>
+  //     <li className="nav-item">
+  //       <a
+  //         href="#about"
+  //         className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+  //       >
+  //         About
+  //       </a>
+  //     </li>
+  //     <li className="nav-item">
+  //       <a
+  //         href="#discover"
+  //         className={props.currentPage === "Discover" ? "nav-link active" : "nav-link"}
+  //       >
+  //         Discover
+  //       </a>
+  //     </li>
+  //     <li className="nav-item">
+  //       <a
+  //         href="#search"
+  //         className={props.currentPage === "Search" ? "nav-link active" : "nav-link"}
+  //       >
+  //         Search
+  //       </a>
+  //     </li>
+  //   </ul>
   )
 }
 
